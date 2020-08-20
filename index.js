ced8b1097e237082ce37a178d7405d6043f274fc
Meredith Strickland <mere.strickland@gmail.com>
2020-08-20 19:37:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//MOVE ELEMENT TO END
+
+function moveElementToEnd(arr, toMove){
+
+  arr.map(num => {
+    if (num === toMove){
+      arr.splice(arr.indexOf(num), 1)
+      arr.push(num)
+    } else num
+  })
+  return arr
+  
+}
+
+moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2)
+//([2, 1, 2, 2, 2, 3, 4, 2], 2)
+//=> [1, 3, 4, 2, 2, 2, 2, 2] 
